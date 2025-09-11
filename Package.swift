5273b2cfe710b81ef52b44eba1b66c6e5cef85c4
William <vimscientist69@gmail.com>
2025-09-11 05:50:17 +0200

    feat(EnvironmentExtensions): refactored vapor environment extensions to this package

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AutomaUtilities",
+    platforms: [
+        .macOS(.v15),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AutomaUtilities",
+            targets: ["AutomaUtilities"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AutomaUtilities",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+            ]
+        ),
+        .testTarget(
+            name: "AutomaUtilitiesTests",
+            dependencies: ["AutomaUtilities"]
+        ),
+    ]
+)
