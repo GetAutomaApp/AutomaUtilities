c1b9a9e6930b66dc2139d62dff65578529b08334
William <vimscientist69@gmail.com>
2025-10-02 12:40:10 +0200

    fixup! fix(MessageService): added metric, created automa utilities metric

--- Sources/AutomaUtilities/DataTypes/AutomaUtilitiesMetric.swift
+++ Sources/AutomaUtilities/DataTypes/AutomaUtilitiesMetric.swift
@@ -12,19 +12,20 @@ internal enum AutomaUtilitiesMetric {
     public static let totalDiscordWebhookMessagesSent = MetricsService.global.makeCounter(
         name: "total_discord_webhook_messages_sent",
         labels: [
-            "status": Self.MetricStatus.success.rawValue,
+            "status": MetricStatus.success.rawValue,
         ]
     )
 
     /// Enum representing the status of a metric.
-    public enum MetricStatus: String, Codable {
-        /// Indicates that the metric already exists.
-        case alreadyExists
-        /// Indicates a failure status for the metric.
-        case fail
-        /// Indicates a start status for the metric.
-        case start
-        /// Indicates a success status for the metric.
-        case success
-    }
+}
+
+public enum MetricStatus: String, Codable {
+    /// Indicates that the metric already exists.
+    case alreadyExists
+    /// Indicates a failure status for the metric.
+    case fail
+    /// Indicates a start status for the metric.
+    case start
+    /// Indicates a success status for the metric.
+    case success
 }
